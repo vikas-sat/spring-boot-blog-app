a767522678fc1eb14bba4d86135128f6ff008484
vikas-sat <73393108+vikas-sat@users.noreply.github.com>
2024-07-16 03:47:24 +0530

    Update SpringMvcBlogAppApplicationTests.java

--- src/test/java/org/martynas/blogapp/SpringMvcBlogAppApplicationTests.java
+++ src/test/java/org/martynas/blogapp/SpringMvcBlogAppApplicationTests.java
@@ -5,9 +5,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringMvcBlogAppApplicationTests {
-
     @Test
     void contextLoads() {
+        // This method is empty because it serves as a placeholder for a basic Spring Boot context loading test.
+        // SpringBootTest annotation ensures that the application context is loaded successfully.
+        // No additional assertions or actions are needed for this test.
     }
-
 }
+
