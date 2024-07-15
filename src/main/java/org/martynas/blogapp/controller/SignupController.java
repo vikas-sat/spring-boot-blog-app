019577d78a023cc1b03f6a79d78fb37e60e418a4
vikas-sat <73393108+vikas-sat@users.noreply.github.com>
2024-07-16 03:53:07 +0530

    Update SignupController.java

--- src/main/java/org/martynas/blogapp/controller/SignupController.java
+++ src/main/java/org/martynas/blogapp/controller/SignupController.java
@@ -1,5 +1,5 @@
 package org.martynas.blogapp.controller;
-
+import java.util.logging.Logger;  // Example for java.util.logging
 import org.martynas.blogapp.model.BlogUser;
 import org.martynas.blogapp.service.BlogUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,7 +22,7 @@ import javax.validation.Valid;
 @Controller
 @SessionAttributes("blogUser")
 public class SignupController {
-
+    private static final Logger logger = Logger.getLogger(SignupController.class.getName());
     private final BlogUserService blogUserService;
 
     @Autowired
@@ -40,26 +40,24 @@ public class SignupController {
 
     @PostMapping("/register")
     public String registerNewUser(@Valid @ModelAttribute BlogUser blogUser, BindingResult bindingResult, SessionStatus sessionStatus) throws RoleNotFoundException {
-        System.err.println("newUser: " + blogUser);  // for testing debugging purposes
+        logger.info("newUser: " + blogUser);  // for testing debugging purposes
         // Check if username is available
         if (blogUserService.findByUsername(blogUser.getUsername()).isPresent()) {
-//            FieldError usernameTakenError = new FieldError("blogUser","username","Username is already registered try other one or go away");
-//            bindingResult.addError(usernameTakenError);
             bindingResult.rejectValue("username", "error.username","Username is already registered try other one or go away");
-            System.err.println("Username already taken error message");
+            logger.info("Username already taken error message");
         }
         // Validate users fields
         if (bindingResult.hasErrors()) {
-            System.err.println("New user did not validate");
+            logger.info("New user did not validate");
             return "registerForm";
         }
         // Persist new blog user
         this.blogUserService.saveNewBlogUser(blogUser);
         // Create Authentication token and login after registering new blog user
         Authentication auth = new UsernamePasswordAuthenticationToken(blogUser, blogUser.getPassword(), blogUser.getAuthorities());
-        System.err.println("AuthToken: " + auth);  // for testing debugging purposes
+        logger.info("AuthToken: " + auth);  // for testing debugging purposes
         SecurityContextHolder.getContext().setAuthentication(auth);
-        System.err.println("SecurityContext Principal: " + SecurityContextHolder.getContext().getAuthentication().getPrincipal());  // for testing debugging purposes
+        logger.info("SecurityContext Principal: " + SecurityContextHolder.getContext().getAuthentication().getPrincipal());  // for testing debugging purposes
         sessionStatus.setComplete();
         return "redirect:/";
     }
